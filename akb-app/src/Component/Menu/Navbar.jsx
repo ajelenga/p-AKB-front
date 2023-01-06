e99d5cea154290730e0cc39cafdd147e9387fcdf
ajelenga <elengaalphonse107@gmail.com>
2023-01-06 14:18:53 +0100

    navbar finsih

--- akb-app/src/Component/Menu/Navbar.jsx
+++ akb-app/src/Component/Menu/Navbar.jsx
@@ -1,29 +1,33 @@
-import {useRef} from "react";
-import  {FaBars, FaTimes } from "react-icons/fa";
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "../../Styles/navbar.css"
 
 function Navbar() {
     const navRef = useRef();
+
     const showNavbar = () => {
         navRef.current.classList.toggle("responsive_nav");
-    }
-    return(
+    };
+
+    return (
         <header>
-            <h3>logo</h3>
+            <h3>LOGO</h3>
             <nav ref={navRef}>
                 <a href="/#">Se Connecter</a>
                 <a href="/#">Des questions</a>
                 <a href="/#">Louez ma voiture</a>
                 <a href="/#">A propos</a>
-                <button className="nav-btn nav-close-btn" onClick={showNavbar}>
-                    <FaTimes/>
-                </button>
-                <button className="nav-btn nav-close-btn" onClick={showNavbar}>
-                    <FaBars/>
+                <button
+                    className="nav-btn nav-close-btn"
+                    onClick={showNavbar}>
+                    <FaTimes />
                 </button>
             </nav>
+            <button className="nav-btn" onClick={showNavbar}>
+                <FaBars />
+            </button>
         </header>
     );
-
 }
 
 export default Navbar;
