3142abc90d3ddcf05ef15770b99b977cccd0b522
ajelenga <elengaalphonse107@gmail.com>
2023-01-13 08:42:07 +0100

    new step

--- akb-app/src/Component/Section/SectionHero.jsx
+++ akb-app/src/Component/Section/SectionHero.jsx
@@ -1,12 +1,11 @@
 import "../../Styles/sectionHero.css"
 import Navbar from "../Menu/Navbar";
-import { AiOutlineDown } from "react-icons/ai";
 
 function SectionHero() {
-   return (
+    return (
         <section className="home">
             <Navbar/>
-            <div className="homepage" >
+            <div className="homepage">
                 <div className="search_hero">
                     <div className="container search_hero__content">
 
