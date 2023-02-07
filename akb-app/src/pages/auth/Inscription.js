eacba10262cda710d1957b714a4d9d06199484ae
ajelenga <elengaalphonse107@gmail.com>
2023-02-07 12:24:30 +0100

    okey modif inscription

--- akb-app/src/pages/auth/Inscription.js
+++ akb-app/src/pages/auth/Inscription.js
@@ -87,6 +87,19 @@ class Inscription extends Component {
                                         </div>
                                     </div>
                                     <div className="grid md:grid-cols-2 md:gap-6">
+                                    <div className="relative z-0 w-full mb-6 group">
+                                        <input type="date" name="floating_company" id="floating_company"
+                                                   className="input-taille block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent
+                                               border-0 border-b-2 border-gray-300 appearance-none dark:text-black
+                                               dark:border-gray-600 dark:focus:border-gray-500 focus:outline-none focus:ring-0
+                                               focus:border-gray-600 peer"
+                                                   placeholder=" " required/>
+                                            <label htmlFor="floating_company"
+                                                   className="peer-focus:font-medium absolute  text-black-500 dark:text-black
+                                               duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0
+                                                peer-focus:text-black-600 peer-focus:dark:text-black-500 peer-placeholder-shown:scale-100
+                                                 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Date de naissance</label>
+                                        </div>
                                         <div className="relative z-0 w-full mb-6 group">
                                             <input
                                                 className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent
@@ -101,19 +114,7 @@ class Inscription extends Component {
                                                 htmlFor="file_input">Photo de profil</label>
 
                                         </div>
-                                        <div className="relative z-0 w-full mb-6 group">
-                                            <input type="date" name="floating_company" id="floating_company"
-                                                   className="input-taille block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent
-                                               border-0 border-b-2 border-gray-300 appearance-none dark:text-black
-                                               dark:border-gray-600 dark:focus:border-gray-500 focus:outline-none focus:ring-0
-                                               focus:border-gray-600 peer"
-                                                   placeholder=" " required/>
-                                            <label htmlFor="floating_company"
-                                                   className="peer-focus:font-medium absolute  text-black-500 dark:text-black
-                                               duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0
-                                                peer-focus:text-black-600 peer-focus:dark:text-black-500 peer-placeholder-shown:scale-100
-                                                 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Date de naissance</label>
-                                        </div>
+                                        
                                     </div>
                                     <div className="grid md:grid-cols-2 md:gap-6">
                                         <div className="relative z-0 w-full mb-6 group">
