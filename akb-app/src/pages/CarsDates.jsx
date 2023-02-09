963cc01114eb9f88f11821acdd12e5f52f758a14
ajelenga <elengaalphonse107@gmail.com>
2023-02-09 16:12:49 +0100

    okey for page

--- akb-app/src/pages/CarsDates.jsx
+++ akb-app/src/pages/CarsDates.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Navbar from "../components/Menu/Navbar";
 import "../Styles/carsDates.sass"
+import { Fragment } from "react";
+import { Typography } from "@material-tailwind/react";
 
 const AllAuto = () => {
     return (
@@ -10,6 +12,11 @@ const AllAuto = () => {
                 <div className="search_hero">
                     <div className="container search_hero__content_login">
                         <div className="home-page-cars rounded overflow-hidden">
+                            <div className="text-center">
+                            <Fragment>
+                            <Typography variant="h5">Liste des véhicules disponiles la date du $*</Typography>
+                            </Fragment>
+                            </div>  
                             <div
                                 className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
                                 <div className="rounded overflow-hidden shadow-lg">
@@ -49,24 +56,6 @@ const AllAuto = () => {
                                     </div>
                                 </div>
 
-                                <div className="rounded overflow-hidden shadow-lg">
-                                    <img className="w-full" src="/forest.jpg" alt="Forest"/>
-                                    <div className="px-6 py-4">
-                                        <div className="font-bold text-xl mb-2">Forest</div>
-                                        <p className="text-gray-700 text-base">
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia,
-                                            Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.
-                                        </p>
-                                    </div>
-                                    <div className="px-6 pt-4 pb-2">
-                                        <span
-                                            className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-                                        <span
-                                            className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-                                        <span
-                                            className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#fall</span>
-                                    </div>
-                                </div>
                                 <div className="rounded overflow-hidden shadow-lg">
                                     <img className="w-full" src="/forest.jpg" alt="Forest"/>
                                     <div className="px-6 py-4">
