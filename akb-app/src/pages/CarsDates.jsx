dc719bb81bc5e4c512c1c3d04a7fd6fe2377cd29
ouaksel <ouakselboukhalfa.ob@gmail.com>
2023-02-10 18:03:32 +0100

    fix display

--- akb-app/src/pages/CarsDates.jsx
+++ akb-app/src/pages/CarsDates.jsx
@@ -1,111 +1,133 @@
-import React, {Fragment, useEffect, useState} from 'react';
+import { Typography } from "@material-tailwind/react";
+import React, { Fragment, useEffect, useState } from "react";
+import { FcApproval } from "react-icons/fc";
+import { useLocation } from "react-router";
 import Navbar from "../components/Menu/Navbar";
-import "../Styles/carsDates.sass"
-import {Typography} from "@material-tailwind/react";
-import {useLocation} from "react-router";
-import {AutoApiF} from '../services/AutoApi';
-import {FcApproval} from "react-icons/fc";
+import { AutoApiF } from "../services/AutoApi";
+import "../Styles/carsDates.sass";
 
 export default function CarsDates() {
-    const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
 
-    const location = useLocation();
-    const test2 = location.state;
-    console.log("test2");
-    console.log(test2);
-    const callApi = () => {
-        const response = AutoApiF(test2);
-        console.log(response);
-    };
+  const location = useLocation();
+  const test2 = location.state;
+  console.log("test2");
+  console.log(test2);
+  const callApi = () => {
+    AutoApiF(test2)
+      .then((response) => {
+        console.log(response.data);
+        setData(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
 
-    useEffect(() => {
-        console.log("useEfect");
-        callApi();
-    }, []);
+  useEffect(() => {
+    console.log("useEfect");
+    callApi();
+  }, []);
 
-    return (
-        <section className="home_LOGIN">
-            <Navbar/>
-            <div className="homepage">
-                <div className="search_hero">
-                    <div className="container search_hero__content_login">
-                        <div className="home-page-cars rounded overflow-hidden">
-                            <div className="text-center">
-                                <Fragment>
-                                    <Typography variant="h5">Liste des véhicules disponiles la date du $*</Typography>
-                                </Fragment>
-                            </div>
-                            <div
-                                className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
-                                {data.map(content => (
-
-                                    <div className="rounded overflow-hidden shadow-lg" key={content.idrsv}>
-                                        <img className="w-full" src="/mountain.jpg" alt="Photo voiture"/>
-                                        <div className="px-6 py-4">
-                                        <span className="flex flex-row justify-start">
-                                            <span><FcApproval/></span>
-                                            <p className="text-gray-600 text-xs ml-1">
-                                            Kilométrage illimité    
-                                            </p>
-                                        </span>
-                                            <span className="flex flex-row justify-start">
-                                            <span><FcApproval/></span>
-                                            <p className="text-gray-600 text-xs ml-1">
-                                            Responsabilité civile   
-                                            </p>
-                                        </span>
-                                            <span class="flex flex-row justify-start">
-                                            <span><FcApproval/></span>
-                                            <p className="text-gray-600 text-xs ml-1">
-                                            Kilométrage illimité  
-                                            </p>
-                                        </span>
-                                            <span className="flex flex-row justify-start">
-                                            <span><FcApproval/></span>
-                                            <p className="text-gray-600 text-xs ml-1">
-                                                Annulation gratuite   
-                                            </p>
-                                        </span>
-                                            <hr class="h-px my-3 bg-gray-100 border-0 dark:bg-gray-300 hr-8"></hr>
-                                            <span className="flex flex-row">
-                                        <span
-                                            className="inline-block bg-gray-200 rounded-full px-3 py-1 text-[10px] font-semibold text-gray-700 mr-2 mb-2">{content.vehicule.boitevitessevcl}</span>
-                                                <span
-                                                    className="inline-block bg-gray-200 rounded-full px-3 py-1 text-[10px] font-semibold text-gray-700 mr-2 mb-2">Climatisation</span>
-                                                <span
-                                                    className="inline-block bg-gray-200 rounded-full px-3 py-1 text-[10px] font-semibold text-gray-700 mr-2 mb-2">4 places</span>
-                                                <span
-                                                    className="inline-block bg-gray-200 rounded-full px-3 py-1 text-[10px] font-semibold text-gray-700 mr-2 mb-2">3 portes</span>
-                                        </span>
-                                            <hr className="h-px my-3 bg-gray-100 border-0 dark:bg-gray-300 hr-8"></hr>
-                                        </div>
-
-                                        <div className="px-6 pt-4 pb-2">
-                                    <span
-                                        className="inline-block  rounded-full  py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Prix: {content.vehicule.prixvcl}€/Kmh</span>
-
-                                            <div className="font-bold text-xl mb-2">
-
-                                                <button
-                                                    className="button-home text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4
-                                        focus:outline-none focus:ring-[#24292F]/50 text-sm font-semibold rounded-lg 
-                                        px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500
-                                        dark:hover:bg-[#050708]/30 mr-2 mb-2  ">Réserver
-                                                </button>
-                                            </div>
-                                        </div>
-
-                                    </div>
-                                ))
-                                }
-
-                            </div>
-
-                        </div>
+  return (
+    <section className="home_LOGIN">
+      <Navbar />
+      <div className="homepage">
+        <div className="search_hero">
+          <div className="container search_hero__content_login">
+            <div className="home-page-cars rounded overflow-hidden">
+              <div className="text-center">
+                <Fragment>
+                  <Typography variant="h5">
+                    Liste des véhicules disponiles la date du $*
+                  </Typography>
+                </Fragment>
+              </div>
+              <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
+                {data.map((content) => (
+                  <div
+                    className="rounded overflow-hidden shadow-lg"
+                    key={content.idTO}
+                  >
+                    <img
+                      className="w-full"
+                      src="/mountain.jpg"
+                      alt="Photo voiture"
+                    />
+                    <div className="px-6 py-4">
+                      <span className="flex flex-row justify-start">
+                        <span>
+                          <FcApproval />
+                        </span>
+                        <p className="text-gray-600 text-xs ml-1">
+                          Kilométrage illimité
+                        </p>
+                      </span>
+                      <span className="flex flex-row justify-start">
+                        <span>
+                          <FcApproval />
+                        </span>
+                        <p className="text-gray-600 text-xs ml-1">
+                          Responsabilité civile
+                        </p>
+                      </span>
+                      <span class="flex flex-row justify-start">
+                        <span>
+                          <FcApproval />
+                        </span>
+                        <p className="text-gray-600 text-xs ml-1">
+                          Kilométrage illimité
+                        </p>
+                      </span>
+                      <span className="flex flex-row justify-start">
+                        <span>
+                          <FcApproval />
+                        </span>
+                        <p className="text-gray-600 text-xs ml-1">
+                          Annulation gratuite
+                        </p>
+                      </span>
+                      <hr class="h-px my-3 bg-gray-100 border-0 dark:bg-gray-300 hr-8"></hr>
+                      <span className="flex flex-row">
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-[10px] font-semibold text-gray-700 mr-2 mb-2">
+                          {content.vehiculeTO.boitevitessevclTO}
+                        </span>
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-[10px] font-semibold text-gray-700 mr-2 mb-2">
+                          Climatisation
+                        </span>
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-[10px] font-semibold text-gray-700 mr-2 mb-2">
+                          4 places
+                        </span>
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-[10px] font-semibold text-gray-700 mr-2 mb-2">
+                          3 portes
+                        </span>
+                      </span>
+                      <hr className="h-px my-3 bg-gray-100 border-0 dark:bg-gray-300 hr-8"></hr>
                     </div>
 
-                </div>
+                    <div className="px-6 pt-4 pb-2">
+                      <span className="inline-block  rounded-full  py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                        Prix: {content.vehiculeTO.prixvclTO}€/Km
+                      </span>
+
+                      <div className="font-bold text-xl mb-2">
+                        <button
+                          className="button-home text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4
+                                        focus:outline-none focus:ring-[#24292F]/50 text-sm font-semibold rounded-lg 
+                                        px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500
+                                        dark:hover:bg-[#050708]/30 mr-2 mb-2  "
+                        >
+                          Réserver
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
             </div>
-        </section>
-    );
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
--- akb-app/src/services/AutoApi.js
+++ akb-app/src/services/AutoApi.js
@@ -1,28 +1,21 @@
-import axios from 'axios';
+import axios from "axios";
 
 //{address_icon: 'rrr', datefield1: '2023-02-10', datefield2: '2023-02-12'}
 export function AutoApiF(credentials) {
-    return axios
-        .post('http://localhost:8083/RequestReservation', {
-            dateDebut: credentials.datefield1,
-            dateFin: credentials.datefield2,
-            addressePrise: credentials.address_icon,
-        })
-        .then(response => {
-            console.log(response.data);
-        })
-        .catch(function (error) {
-            console.error(error);
-        });
+  return axios.post("http://localhost:8082/RequestReservation", {
+    dateDebut: credentials.datefield1,
+    dateFin: credentials.datefield2,
+    addressePrise: credentials.address_icon,
+  });
 }
 
 export function AutoApiFTest() {
-    return axios
-        .get('https://jsonplaceholder.typicode.com/posts')
-        .then(response => {
-            console.log(response.data);
-        })
-        .catch(function (error) {
-            console.error(error);
-        });
+  return axios
+    .get("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
 }
