df49207699df6c285f9480faf479562b6f005166
ajelenga <elengaalphonse107@gmail.com>
2023-03-03 12:00:52 +0100

    okey

--- akb-app/src/pages/PageError.jsx
+++ akb-app/src/pages/PageError.jsx
@@ -13,15 +13,16 @@ const PageError = () => {
                             <section className="flex items-center h-full p-16 ">
                                 <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
                                     <div className="max-w-md text-center">
-                                        <h2 className="mb-8 font-extrabold text-9xl dark:text-gray-600">
+                                        <h2 className="mb-8 font-extrabold text-9xl text-gray-900 dark:text-black">
                                             <span className="sr-only">Error</span>404
                                         </h2>
                                         <p className="text-2xl font-semibold md:text-3xl">Désolé, nous n'avons pas pu
                                             trouver cette page.</p>
-                                        <p className="mt-4 mb-8 dark:text-gray-400">Mais ne vous inquiétez pas, vous
+                                        <p className="mt-4 mb-8 text-gray-900 dark:text-black">Mais ne vous inquiétez
+                                            pas, vous
                                             pouvez trouver beaucoup d'autres choses sur notre page d'accueil.</p>
                                         <button
-                                            className="px-8 py-3 font-semibold rounded dark:bg-gray-400 dark:text-gray-900"
+                                            className="px-8 py-3 font-semibold rounded text-white bg-[#24292F] hover:bg-[#24292F]/90"
                                             onClick={() => navigate("/")}>Retour à la page d'accueil
                                         </button>
                                     </div>
